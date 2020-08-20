01e93c8ac0e68435dd4a1a1bc1da4e5b86c55d13
Chain <chain.the.net@gmail.com>
2020-08-21 00:57:55 +0700

    My first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 8080
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+	res.status(200).send('Server is working.')
+})
+
+app.listen(port, () => {
+	console.log(`🌏 Server is running at http://localhost:${port}`)
+})
