3f50fc78500f6ff2bfdf78a422026adea124f3e0
Chain <chain.the.net@gmail.com>
2020-08-21 01:23:43 +0700

    My first commit

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = express()
-
 const port = process.env.PORT || 8080
+const dfff = require('dialogflow-fulfillment')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-	res.status(200).send('Server is working.')
+	res.status(200).send('Server is working now.')
+})
+
+app.post('/', (req, res)=>{
+    const agent = new dfff.WebhookClient({
+        request : req,
+        response : res
+    })
+
+    function demo(agent){
+        agent.add("Sending response from Webhook server")
+    }
+
+    var intentMap = new Map()
+
+    intentMap.set('webhookDemo', demo)
+
+    agent.handleRequest(intentMap)
 })
 
 app.listen(port, () => {
