dd1094d84a39cf85c23f84604df1159262d91ff3
Chain <chain.the.net@gmail.com>
2020-08-21 16:57:04 +0700

    My first commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 const dfff = require('dialogflow-fulfillment')
+const { BasicCard } = require('actions-on-google')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -24,28 +25,21 @@ app.post('/', (req, res)=>{
     function cek_kebutuhan(agent){
         const umur = req.body.queryResult.parameters["umur_user"]
         const status = agent.parameters.status_pernikahan
-        var payloadCard = {
-            "richContent": [
-                {
-                    "type": "accordion",
-                    "title": "Accordion title",
-                    "subtitle": "Accordion subtitle",
-                    "image": {
-                        "src": {
-                            "rawUrl": "https://example.com/images/logo.png"
-                        }
-                    },
-                    "text": "Accordion text"
-                }
-            ]
-        }
 
-        if (umur <= 21 && status == "single")
+        if (umur <= 21 && status == "single"){
         agent.add("Kamu bisa mulai dari menabung dulu, baik di bank atau di celengan pribadi. Pastikan kamu mulai menabung, berapapun nominalnya.")
         agent.add("Selain itu, mulailah mencatat setiap pengeluaranmu, baik jajan gorengan di sekolah atau di kampus, agar kebiasaan mencatat bisa terbentuk.")
         agent.add("Kamu bisa pakai aplikasi seperti Money Lover atau Money Manager untuk memudahkan kamu.")
-        agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadCard), {sendAsMessage: true, rawPayload: true})
-            
+        agent.add(new dfff.Card({
+            title: 'hello',
+            text: 'test',
+            imageUrl: 'https://goo.gl/aeDtrS',
+            buttonText: 'Details',
+            buttonUrl: 'https://assistant.google.com/'
+        
+        }))
+        
+        }    
         console.log(`umur = ${umur} dan status = ${status}` )   
     }
 
