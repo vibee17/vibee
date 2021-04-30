2fc5dd8cb773a0eff56c426a87f03cdbc9bdf9e7
Chain <chain.the.net@gmail.com>
2021-05-01 05:36:34 +0700

    add

--- index.js
+++ index.js
@@ -54,14 +54,20 @@ app.post('/', (req, res)=>{
 		sendAsMessage: true
 		})
 		
-		if (rasio_dana_darurat < 3) {
-			agent.add('Berdasarkan perhitungan VIRA, rasio dana darurat kamu adalah ' + rasio_dana_darurat + '. Buat yang berstatus single, rasio dana darurat yang ideal adalah 3 ke atas. Yuk bisa yuk!')
+		if (status == "belum" && rasio_dana_darurat < 3) {
+			agent.add('Berdasarkan perhitungan VIRA, rasio dana darurat kamu adalah ' + rasio_dd_bulat + '. Buat yang berstatus single, rasio dana darurat yang ideal adalah 3 ke atas. Yuk bisa yuk!')
 			agent.add(`VIRA punya beberapa saran buat kamu yang mau mulai kumpulin dana darurat: 
 1. Langsung sisihkan minimal 10% pendapatan setelah bayar semua kewajiban
 2. Coba kurangi pengeluaran yang tidak terlalu mendesak ya
 3. Dana darurat bisa disimpan di tabungan terpisah dan pastiin bisa diambil kapan aja dibutuhkan, misalnya di Tahapan BCA. Kalau kamu belum punya, sekarang buka rekening gak harus ke kantor cabang, kamu bisa buka rekening lewat aplikasi BCA mobile. 
-Lihat info lengkapnya di sini http://bca.id/virabukarekening `)
-			
+Lihat info lengkapnya di sini https://bca.id/virabukarekening`)
+			agent.add(payloaddana_darurat)
+		}
+		
+				if (status == "belum" && rasio_dana_darurat >= 3) {
+			agent.add('Menurut VIRA, dana daruratmu udah cukup optimal kok yaitu di angka rasio ' + rasio_dd_bulat + '. Buat yang berstatus single, rasio yang ideal adalah 3 ke atas. Nah pastiin aja dana tersebut bisa kamu ambil kapan aja saat dibutuhkan, bisa juga disimpan di tabungan, misalnya Tahapan BCA.')
+			agent.add('Kalau kamu belum punya Tahapan BCA, sekarang buka rekening gak harus ke kantor cabang lho. Kamu bisa langsung buka rekening lewat aplikasi BCA mobile.  Lihat info lengkapnya di sini https://bca.id/virabukatabungan')
+			agent.add(payloaddana_darurat)
 		}
 		
 		console.log(`dana tunai = ${dana_tunai}, pengeluaran = ${pengeluaran}, rasio dana darurat = ${rasio_dd_bulat}, status = ${status}` )
