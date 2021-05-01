ccdb6a47dfdf7ab81624994cd9d409a5291865b7
Chain <chain.the.net@gmail.com>
2021-05-01 11:10:30 +0700

    add

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/', (req, res)=>{
 		const rasio_dana_darurat = dana_tunai / pengeluaran;
 		const rasio_dd_bulat = rasio_dana_darurat.toFixed(2);
 		
-		const buttoncekup = {
+		const buttoncekup_dd = {
 		"type": "template",
 		"altText": "Kriteria lainnya",
 		"template": {
@@ -54,26 +54,7 @@ app.post('/', (req, res)=>{
 					}
 		};
 
-		let button_cek_up_dd = new Payload(agent.LINE, {		
-		"type": "template",
-		"altText": "Kriteria lainnya",
-		"template": {
-			"type": "buttons",
-			"text": "Cek kondisi finansial lainnya",
-			"actions": [
-				{
-					"type": "message",
-					"label": "Cicilan",
-					"text": "Cicilan"
-				},
-				{
-					"type": "message",
-					"label": "Dana Investasi",
-					"text": "Dana Investasi"
-				}
-						]
-					}
-		}, { sendAsMessage : true, rawPayload: false });
+		let button_cek_up_dd = new Payload(agent.LINE, buttoncekup_dd, { sendAsMessage : true, rawPayload: false });
 		
 		if (status == "belum") { 
 			if (rasio_dana_darurat < 3) {
@@ -93,7 +74,7 @@ Lihat info lengkapnya di sini https://bca.id/virabukarekening`)
 		
 		else if (status == "sudahxanak") {
 			if (rasio_dana_darurat < 9) {
-			agent.add('Menurut VIRA, dana daruratmu udah cukup optimal kok yaitu di angka rasio ' + rasio_dd_bulat + '. Buat yang berstatus menikah dan belum punya anak, rasio dana darurat yang ideal adalah 9 ke atas. Yuk bisa yuk!')
+			agent.add('Berdasarkan perhitungan VIRA, rasio dana darurat kamu adalah ' + rasio_dd_bulat + '. Buat yang berstatus menikah dan belum punya anak, rasio dana darurat yang ideal adalah 9 ke atas. Yuk bisa yuk!')
 			agent.add(`VIRA punya beberapa saran buat kamu yang mau mulai kumpulin dana darurat: 
 1. Langsung sisihkan minimal 10% pendapatan setelah bayar semua kewajiban
 2. Coba kurangi pengeluaran yang tidak terlalu mendesak ya
@@ -109,7 +90,7 @@ Lihat info lengkapnya di sini https://bca.id/virabukarekening`)
 		
 		else if (status == "sudahanak") {
 			if (rasio_dana_darurat < 12) {
-			agent.add('Menurut VIRA, dana daruratmu udah cukup optimal kok yaitu di angka rasio ' + rasio_dd_bulat + '. Buat yang berstatus menikah dan sudah punya anak, rasio dana darurat yang ideal adalah 12 ke atas. Yuk bisa yuk!')
+			agent.add('Berdasarkan perhitungan VIRA, rasio dana darurat kamu adalah ' + rasio_dd_bulat + '. Buat yang berstatus menikah dan sudah punya anak, rasio dana darurat yang ideal adalah 12 ke atas. Yuk bisa yuk!')
 			agent.add(`VIRA punya beberapa saran buat kamu yang mau mulai kumpulin dana darurat: 
 1. Langsung sisihkan minimal 10% pendapatan setelah bayar semua kewajiban
 2. Coba kurangi pengeluaran yang tidak terlalu mendesak ya
@@ -128,11 +109,58 @@ Lihat info lengkapnya di sini https://bca.id/virabukarekening`)
 		console.log(`dana tunai = ${dana_tunai}, pengeluaran = ${pengeluaran}, rasio dana darurat = ${rasio_dd_bulat}, status = ${status}` );
 	}	
 
+	function cek_cicilan(agent) {
+
+		const utang = agent.parameters.total_cicilan;
+		const pendapatan = agent.parameters.total_pendapatan;		
+		
+		const rasio_utang = utang / pendapatan;
+		const rasio_dd_utang = rasio_utang.toFixed(2);
+		
+		const buttoncekup = {
+		"type": "template",
+		"altText": "Kriteria lainnya",
+		"template": {
+			"type": "buttons",
+			"text": "Cek kondisi finansial lainnya",
+			"actions": [
+				{
+					"type": "message",
+					"label": "Dana Darurat",
+					"text": "Dana darurat"
+				},
+				{
+					"type": "message",
+					"label": "Dana Investasi",
+					"text": "Dana Investasi"
+				}
+						]
+					}
+		};
+
+		let button_cek_up_cicilan = new Payload(agent.LINE, buttoncekup, { sendAsMessage : true, rawPayload: false });		
+
+		if (rasio_utang < 0.3) { 
+		agent.add(`Nah, menurut VIRA kamu sudah cukup oke kok dalam mengatur porsi cicilan. Yeay!
+Jangan lupa untuk mengontrol cicilanmu supaya tidak lebih dari 30% dari pendapatanmu. Lalu, sisihkan juga buat investasi biar tabungan kamu gak terkikis inflasi.`)
+		agent.add('Kalau kamu mau investasi di obligasi atau reksadana secara online bisa lewat aplikasi WELMA. Lihat info lengkapnya di sini https://bca.id/infolengkapwelma')			
+		}
+		
+		else if (rasio_utang >= 0.3) {
+		agent.add('Buat kamu, sebaiknya mulai pelan-pelan nurunin jumlah cicilan tiap bulannya. Maksimal 30% aja. Ayo semangaat, kamu pasti bisa!')
+		agent.add(`Beberapa saran dari VIRA: 
+1. Sebelum ngambil cicilan baru, coba pertimbangkan lagi apakah itu kebutuhan atau keinginan.
+2. Coba prioritaskan untuk melunasi cicilan kartu kredit kamu dengan mengurangi alokasi pengeluaran yang kurang mendesak yaa.`)
+		}
+		agent.add(button_cek_up_cicilan);
+		
+	}
+
     var intentMap = new Map()
 
     intentMap.set('webhookDemo', demo)
 	intentMap.set('cek.up.gen.dana.darurat', cek_dana_darurat)
-
+	intentMap.set('cek.up.gen.cicilan', cek_cicilan)
     agent.handleRequest(intentMap)
 })
 
